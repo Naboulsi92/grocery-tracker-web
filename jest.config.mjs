fd01dab84e59001b3d5c4edbbfbf7959f42ce6ad
Naboulsi92 <naboulsi.riyad@gmail.com>
2026-09-21 18:57:52 +0100

    chore: CI honnete lint scope AGENTS S6 (Closes #116) (#135)
    * chore: CI honnete lint scope AGENTS S6 (Closes #116)
    
    * fix: refresh stale counts post-rebase (30/358, e2e 237/17) (#116)
    
    ---------
    
    Co-authored-by: Riyad NABOULSI <riyad.naboulsi@sofrecom.com>

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({ dir: './' });
 export default createJestConfig({
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testPathIgnorePatterns: ['[/\\\\]src[/\\\\]e2e[/\\\\]'],
+  testPathIgnorePatterns: ['[/\\\\]src[/\\\\]e2e[/\\\\]', '<rootDir>/scripts/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
